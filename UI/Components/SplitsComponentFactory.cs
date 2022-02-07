3b2cb2bbd01f8cb496c87d1e714a7fa47161258d
Ian C <31007296+Minibeast@users.noreply.github.com>
2022-02-06 23:11:09 -0500

    first

---
+++ UI/Components/SplitsComponentFactory.cs
@@ -0,0 +1,24 @@
+﻿using LiveSplit.Model;
+using System;
+
+namespace LiveSplit.UI.Components
+{
+    public class SplitsComponentFactory : IComponentFactory
+    {
+        public string ComponentName => "Splits w/ Counter";
+
+        public string Description => "Splits with a Counter. Made by Minibeast.";
+
+        public ComponentCategory Category => ComponentCategory.List;
+
+        public IComponent Create(LiveSplitState state) => new SplitsComponent(state);
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "";
+
+        public string UpdateURL => "";
+
+        public Version Version => Version.Parse("1.8.10");
+    }
+}
